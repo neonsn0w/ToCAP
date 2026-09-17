64e77dd36dde34a771d55204dc50f2da1006a0ae
neonsn0w <pietro@eprase.com>
2026-09-17 14:13:36 +0200

    File saving

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
+import os
+
 from flask import Flask, render_template, request
+from pathlib import Path
+from dotenv import load_dotenv
 
 from cookie_checker import check_instagram_cookies
 app = Flask(__name__)
 
+load_dotenv()
+
+COOKIES_FOLDER_PATH = os.getenv("COOKIES_FOLDER_PATH")
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    result = None
+
     if request.method == "POST":
         cookies = request.form.get("cookies_textbox")
 
-        mlep = check_instagram_cookies(cookies)
+        result = check_instagram_cookies(cookies)
 
-    return render_template("index.html", result = mlep)
+        if result["status"] == "VALID":
+            file = Path(COOKIES_FOLDER_PATH + result["username"] + ".txt")
+            if not file.exists():
+                with open(file, "w") as f:
+                    f.write(cookies)
+                    f.flush()
+            else:
+                result = {
+                    "status": "DUPLICATE",
+                    "message": "Valid but already added"
+                }
 
-
+    return render_template("index.html", result = result)
 
     return render_template('index.html', processed = processed)
 
